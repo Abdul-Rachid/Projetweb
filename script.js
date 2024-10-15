07932c3e882cb9f504cc2de92f588bdb51fed41a
Abdoul Rachid <search3for4all@gmail.com>
2024-10-15 04:01:24 +0000

    Version 1.03 : améliorée avec menu déroulant

---
+++ script.js
@@ -0,0 +1,7 @@
+
+/* Je vérifie si le script s'exécute après le chargement du DOM
+document.addEventListener('DOMContentLoaded', function () {
+    const menuItems = document.querySelector('.vector-menu-content-list');
+});  */
+
+// Overlay
